c91267ab1cfa53efa728f09550bcac175cad35c0
choiseungwon <b088081@gmail.com>
2020-03-17 04:40:11 +0900

    Spring Jpa 구현

---
+++ src/test/java/com/swchoi/webservice/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.swchoi.webservice.springboot.domain.posts;
+
+import com.swchoi.webservice.springboot.domain.posts.Posts;
+import com.swchoi.webservice.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_블러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                                    .title(title)
+                                    .content(content)
+                                    .author("b088081@gmail.com")
+                                    .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
