32a84f10d08cadab47fbe9ae0e431e98aa2bf421
choiseungwon <b088081@gmail.com>
2020-03-17 21:39:32 +0900

    Mustache로 화면 구성하기
    - 기본 페이지 만들기

--- src/main/java/com/swchoi/webservice/springboot/domain/posts/Posts.java
+++ src/main/java/com/swchoi/webservice/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.swchoi.webservice.springboot.domain.posts;
 
+import com.swchoi.webservice.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/swchoi/webservice/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/swchoi/webservice/springboot/domain/posts/PostsRepositoryTest.java
@@ -2,6 +2,7 @@ package com.swchoi.webservice.springboot.domain.posts;
 
 import com.swchoi.webservice.springboot.domain.posts.Posts;
 import com.swchoi.webservice.springboot.domain.posts.PostsRepository;
+import jdk.vm.ci.meta.Local;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -44,5 +46,30 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2020,03,17,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>> createDate="+posts.getCreateDate()
+                +", modeifeidDate="+posts.getModifiedDate());
+
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 }
