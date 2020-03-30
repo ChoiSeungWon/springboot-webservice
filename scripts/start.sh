fd31af8f5cc9d48a7aab9e11134a8b81c6943916
choiseungwon <b088081@gmail.com>
2020-03-30 20:32:58 +0900

    수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=freelec-springboot2-webservice
+PROJECT_NAME=springboot-webservice
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
