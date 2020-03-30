32c5c33f84bbc28cd280c94837fe9540499a1992
choiseungwon <b088081@gmail.com>
2020-03-30 20:11:34 +0900

    profile.sh 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -23,7 +23,7 @@ function find_idle_profile(){
 }
 
 # 쉬고 있는 profile의 port 찾기
-function find_idel_port() {
+function find_idle_port() {
     IDLE_PROFILE=$(find_idle_profile)
 
     if [ ${IDLE_PROFILE} == real1 ]
