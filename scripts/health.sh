a8987554325aeb3a350150b58131f2203a08c796
choiseungwon <b088081@gmail.com>
2020-03-30 20:20:04 +0900

    nginx 무중단 배포 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -3,13 +3,13 @@
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
-SOURCE ${ABSDIR}/switch.sh
+source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -s http://localhost:$IDLE_PORT/profile"
+echo "> curl -s http://localhost:$IDLE_PORT/profile "
 sleep 10
 
 for RETRY_COUNT in {1..10}
@@ -18,13 +18,13 @@ do
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
-  then #$up_count >= 1 ("real" 문자열이 있는지 검증)
-    echo "> Health check 성공"
-    switch_proxy
-    break
+  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
+      echo "> Health check 성공"
+      switch_proxy
+      break
   else
-    echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health check: ${RESPONSE}"
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
--- scripts/start.sh
+++ scripts/start.sh
@@ -5,17 +5,17 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=springboot-webservice
+PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR Name : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
@@ -28,6 +28,6 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 nohup java -jar \
-   -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-   -Dspring.profiles.active=$IDLE_PROFILE \
-   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
