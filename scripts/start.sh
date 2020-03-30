ee48c9ffad27d704a9d5f58264ad8a506df89e18
choiseungwon <b088081@gmail.com>
2020-03-30 21:28:18 +0900

    start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,12 +7,17 @@ source ${ABSDIR}/profile.sh
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=springboot-webservice
 
+IDLE_PROFILE=$(find_idle_profile)
+
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME_TEMP=`echo $JAR_NAME | cut -c1-37`
+mv $JAR_NAME $JAR_NAME_TEMP$IDLE_PROFILE.jar
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
@@ -22,8 +27,6 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
-IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
